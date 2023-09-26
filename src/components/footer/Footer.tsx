47eeed2106507d172da9d1d221069591ff317aaf
ulugbekmahammadjonov <ulugbekmahammadjonov1218@gmail.com>
2023-09-26 15:20:11 +0500

    first commit

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>lamadmin</span>
+      <span>© Mdevs | Admin Dashboard</span>
+    </div>
+  )
+}
+
+export default Footer
